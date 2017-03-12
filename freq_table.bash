ea11d05464166b97e09ba0ca523d5e99ec03fd29
Dmitri Chubarov <dchubarov@acm.org>
2017-03-13 06:19:27 +0700

    filter out urls

--- freq_table.bash
+++ freq_table.bash
@@ -1,5 +1,6 @@
 filename=$1
 basename=${filename%.text}
 
-tr ' ' '\n' < ${filename} | tr -d '.,!#"' | sed 's/[«»]//g ; s/^\(.*\)$/\L\1/'  | sort | uniq -c | sort -rn -k1,1 | tee ${basename}.freq
+sed 's/http:[^ ]*//' < ${filename} | tr ' ' '\n' | tr -d '.,!#"' | sed 's/[«»]//g ; s/^\(.*\)$/\L\1/'  |\
+    sort | uniq -c | sort -rn -k1,1 | tee ${basename}.freq
 
